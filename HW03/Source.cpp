8a07134992a1c3b6198739ca37d0d1350b347555
Xuan_Phat <50132895+XuanPhat0603@users.noreply.github.com>
2020-07-18 22:37:52 +0700

    lan 1

---
+++ HW03/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "MyString.h"
+using namespace std;
+int main()
+{
+	MyString s, t;
+	cin >> s >> t;
+	cout << s <<endl << t << endl;
+	cout << s.length() << endl;
+	cout << s.subString(2, 3) << endl;
+	cout << s + t << endl;
+//	cout << s.insert(1, "!") <<endl;
+//	cout << s.erase(0, 2)<< endl;
+//	cout << s.replace(0, "abc")<< endl;
+	//cout << s.find(0, "ll");
+}
